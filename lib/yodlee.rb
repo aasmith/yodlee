c596f1d2c92f94b07336693e8a748f916d16963c
John Barnette <jbarnette@gmail.com>
2009-02-03 02:45:01 +0800

    Move monkeypatches into their own file.
    Signed-off-by: aasmith <andy@tinnedfruit.org>

--- lib/yodlee.rb
+++ lib/yodlee.rb
@@ -3,17 +3,8 @@ require 'mechanize'
 require 'nokogiri'
 require 'enumerator'
 
+require 'yodlee/monkeypatches'
 require 'yodlee/version'
-
-class String
-  def underscore
-    gsub(/::/, '/').
-      gsub(/([A-Z]+)([A-Z][a-z])/,'\1_\2').
-      gsub(/([a-z\d])([A-Z])/,'\1_\2').
-      tr("-", "_").
-      downcase
-  end unless respond_to?(:underscore)
-end
 
 module Yodlee
   class Credentials
